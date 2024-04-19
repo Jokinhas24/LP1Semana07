86de8e1f559e16aa8fb85859ed291f94fee5205e
João Nogueira <joaonogueira.art@gmail.com>
2024-04-19 13:35:15 +0100

    Modify SpyUnit and crete the GetInfo method

--- GameUnits/SpyUnit.cs
+++ GameUnits/SpyUnit.cs
@@ -7,6 +7,25 @@ namespace GameUnits
 {
     public class SpyUnit : XPUnit
     {
-        
+        public override float Cost  { get; }
+        public SpyUnit(int mov = 8, int health = 2) : base(mov, health)
+        {
+            Cost = 12.5f;
+        }
+        public void GetSecretInfo(Unit unit)
+        {
+            if (unit is SpyUnit)
+            {
+                XP += 3;
+            }
+            else if (unit is MilitaryUnit)
+            {
+                XP += 2;
+            }
+            else
+            {
+                XP += 1;
+            }
+        }
     }
 }
