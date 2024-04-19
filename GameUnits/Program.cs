8e84a793dc06a5ab72fb1a8a6ee3043e522731c6
João Nogueira <joaonogueira.art@gmail.com>
2024-04-19 01:06:31 +0100

    Tested both instances of the units

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace GameUnits
 {
@@ -6,7 +7,13 @@ namespace GameUnits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Unit Sniper = new MilitaryUnit(2, 2, 10);
+            Sniper.Move();
+            Console.WriteLine($"Sniper has {Sniper.Health} and costs {Sniper.Cost} gold.");
+
+            Unit Construct = new SettlerUnit();
+            Construct.Move();
+            Console.WriteLine($"Construct has {Construct.Health} and costs {Construct.Cost} gold.");
         }
     }
 }
