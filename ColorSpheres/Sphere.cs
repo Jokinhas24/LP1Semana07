435c2c1134ce763cb213d17ebd8e83d547a79270
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-04-23 15:23:08 +0100

    Added color contructer

---
+++ ColorSpheres/Sphere.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorSpheres
+{
+    public class Sphere
+    {
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
+        public Color(int red, int green, int blue, int alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            alpha = 255;
+        }
+
+        public int GetRed()
+        {
+            return red;
+        }
+        public int GetGreen()
+        {
+            return green;
+        }
+        public int GetBlue()
+        {
+            return blue;
+        }
+        public int GetAlpha()
+        {
+            return alpha;
+        }
+        public int GetGrey()
+        {
+            return (red + green + blue)/3;
+        }
+    }
+}
